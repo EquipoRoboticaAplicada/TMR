34b4220f0090561b8dfd014d724bf65dbd5724ea
LivingCheerios <bhjf28dos@gmail.com>
2026-04-11 03:25:52 -0600

    xd
    xd

--- jetson/command.py
+++ jetson/command.py
@@ -19,7 +19,7 @@ class Route_Command:
     def follow_path(self, rover_odometry):
         DIST_TOLERANCE  = 0.2   # m   — distancia para considerar que se llegó al punto
         ANGLE_TOLERANCE = 0.1   # rad — error angular antes de avanzar recto
-        BASE_RPM        = 45
+        BASE_RPM        = 55
 
         FWD  = "D1"
         BWD  = "D0"
