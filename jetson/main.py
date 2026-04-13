453f197f3904fb3cc1aa0715b3096567aff7083e
David D. <daizpaz19@gmail.com>
2026-04-12 23:35:44 -0600

    debug

--- jetson/main.py
+++ jetson/main.py
@@ -9,7 +9,7 @@ from odo import RoverOdometry
 from local_debug import run_debug
 import threading
 
-DRAW_LOCAL = False   # ← cambiar a False para desactivar la ventana de debug
+DRAW_LOCAL = True   # ← cambiar a False para desactivar la ventana de debug
 
 
 if __name__ == "__main__":
