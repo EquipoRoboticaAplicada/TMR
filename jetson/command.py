c7806f8e5e65f9cdad2feb69838dfb6b7b948181
David D. <daizpaz19@gmail.com>
2026-04-16 17:25:32 -0600

    debug off

--- jetson/command.py
+++ jetson/command.py
@@ -82,8 +82,8 @@ class Route_Command:
         correction        = self.cf_alpha * odom_delta + (1.0 - self.cf_alpha) * imu_delta
         self._fused_theta = normalize_angle(self._fused_theta + correction)
 
-        print(f"[CF] Odom θ: {math.degrees(odom_theta):.2f}° rad, IMU θ: {math.degrees(imu_theta):.2f}° rad, "
-              f"Fused θ: {math.degrees(self._fused_theta):.2f}° rad")
+        # print(f"[CF] Odom θ: {math.degrees(odom_theta):.2f}° rad, IMU θ: {math.degrees(imu_theta):.2f}° rad, "
+        #       f"Fused θ: {math.degrees(self._fused_theta):.2f}° rad")
 
         return self._fused_theta
 
