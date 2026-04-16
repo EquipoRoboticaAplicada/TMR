6316315f8459de0bc92d6d238db1ff8fd7aa1ed8
TPO-FRAN <diego.franco@udem.edu>
2026-04-16 15:32:21 -0600

    debug HMI

--- Wifi/HMI/HMI.py
+++ Wifi/HMI/HMI.py
@@ -1,4 +1,6 @@
 import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import math
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QOpenGLWidget
 from PyQt5.QtGui import QPainter, QColor, QPen, QPolygonF, QLinearGradient
