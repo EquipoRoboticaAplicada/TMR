109741974b6db6a78f0a5078fac05b0cf23e859a
LivingCheerios <bhjf28dos@gmail.com>
2026-04-17 11:41:05 -0600

    cambios
    cambios para que la odo sea solo con las primeras 2 llantas

--- jetson/connect.py
+++ jetson/connect.py
@@ -219,7 +219,7 @@ class ESP:
                 seq    = int(parts[1])
                 m_data = [
                     {"rpm": float(parts[2]), "m/s": float(parts[3])},
-                    {"rpm": float(parts[4]), "m/s": float(parts[5])},
+                   # {"rpm": float(parts[4]), "m/s": float(parts[5])},
                     # {"rpm": float(parts[6]), "m/s": float(parts[7])}, # Solo 2 motores por lado, no 3. Encoders muertos. 
                 ]
 
